78f2875e299c756145d741d2c9097a3159b65f05
Thejus <Git@tigerraw22.anonaddy.com>
2021-11-18 18:02:04 +0530

    made a model website

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "login"
+    return render_template('base.html')
 
 @auth.route('/logout')
 def logout():
