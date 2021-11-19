f44769cb4048cd1a4d6ae41df48df39a54b6a04e
Thejus <Git@tigerraw22.anonaddy.com>
2021-11-19 06:36:04 +0530

    creating user done now to create loginin

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint,render_template,request,flash
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -26,6 +29,14 @@ def signup():
             flash('Password don\'t match :(', category='error')        
         else :
             # add user to database 
+            new_user = User(email=email,first_name=fname,password=generate_password_hash(password1,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('Account created', category='success')
+            return redirect(url_for('views.home'))
+
+
+
 
     return render_template('signup.html')
